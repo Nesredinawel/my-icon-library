c06aa160bd7eb8fdcdf1d68e89efd671d294a78c
Nesredinawel <nesredina35@gmail.com>
2026-04-29 15:46:59 +0300

    quick fix

---
+++ packages/icons-flutter/CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## 1.0.0
+
+- Initial release
+- Outline icons
+- Solid icons
+- Duotone icons
+- Monochrome icons
+- Custom size and color support
--- packages/icons-flutter/lib/src/base_icon.dart
+++ packages/icons-flutter/lib/src/base_icon.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+/// Base widget used internally by all generated icons.
+///
+/// This widget renders an SVG string using [flutter_svg].
+///
+/// Example:
+/// ```dart
+/// OutlineHome(size: 32, color: Colors.blue);
+/// ```
 class NasIcon extends StatelessWidget {
+  /// Raw SVG string content.
   final String svg;
+
+  /// Icon size (width and height).
+  ///
+  /// Defaults to 24.
   final double size;
+
+  /// Icon color.
+  ///
+  /// If null, the SVG uses its default color.
   final Color? color;
 
+  /// Creates a Nasicon widget.
   const NasIcon({
     super.key,
     required this.svg,
